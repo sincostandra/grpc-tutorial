d7c9a1cf5f1c2a4ba46d496004d5db2690800322
sincostandra <utandraemail@gmail.com>
2025-05-06 14:43:38 +0700

    Payment Service Implementation

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
-fn main() -> Result<(), Box<dyn std::error:Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
         .compile(
